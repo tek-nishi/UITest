45109e039bce2222811d7358ac7ed91c04f792ae
Nishiyama Nobuyuki <tek.nishi@gmail.com>
2017-02-22 18:36:49 +0900

    不必要なboost::optionalを削除

--- src/Tween.hpp
+++ src/Tween.hpp
@@ -15,7 +15,7 @@ class Tween
   ci::EaseFn ease_;
 
   boost::optional<float> start_;
-  boost::optional<float> end_;
+  float end_;
 
   float duration_;
   boost::optional<float> delay_;
@@ -27,6 +27,7 @@ class Tween
 public:
   Tween(const ci::JsonTree& params) noexcept
     : ease_(getEaseFunc(params.getValueForKey<std::string>("type"))),
+      end_(params.getValueForKey<float>("end")),
       duration_(params.getValueForKey<float>("duration")),
       ping_pong_(Json::getValue(params, "ping_pong", false)),
       loop_(Json::getValue(params, "loop", false))
@@ -34,10 +35,6 @@ public:
     if (params.hasChild("start"))
     {
       start_ = params.getValueForKey<float>("start");
-    }
-    if (params.hasChild("end"))
-    {
-      end_ = params.getValueForKey<float>("end");
     }
 
     if (params.hasChild("delay"))
@@ -48,8 +45,8 @@ public:
 
   void apply(const ci::TimelineRef& timeline, float* target) noexcept
   {
-    auto option = start_ ? timeline->applyPtr(target, *start_, *end_, duration_, ease_)
-                         : timeline->applyPtr(target, *end_, duration_, ease_);
+    auto option = start_ ? timeline->applyPtr(target, *start_, end_, duration_, ease_)
+                         : timeline->applyPtr(target, end_, duration_, ease_);
 
     if (delay_) option.delay(*delay_);
     option.loop(loop_);
@@ -58,8 +55,8 @@ public:
   
   void append(const ci::TimelineRef& timeline, float* target) noexcept
   {
-    auto option = start_ ? timeline->appendToPtr(target, *start_, *end_, duration_, ease_)
-                         : timeline->appendToPtr(target, *end_, duration_, ease_);
+    auto option = start_ ? timeline->appendToPtr(target, *start_, end_, duration_, ease_)
+                         : timeline->appendToPtr(target, end_, duration_, ease_);
 
     if (delay_) option.delay(*delay_);
     option.loop(loop_);
